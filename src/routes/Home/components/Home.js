90354f1b459f91e5f79737991c76bc0e0cd7de5e
Ahmed ayman <ahmedayman.aa191@gmail.com>
2019-06-14 08:56:15 +0200

    show map by import MapView component and then run-android command to run two apps in the gradle  with each other  .

--- src/routes/Home/components/Home.js
+++ src/routes/Home/components/Home.js
@@ -12,25 +12,34 @@ class Home extends React.Component{
 	render(){
 
 		return(
-			 <View style={styles.container}>
-			 
-<Text>Hello {this.props.name} </Text>
-   </View>
+			<View style={styles.container}>
+    		<MapView
+					provider={PROVIDER_GOOGLE} // remove if not using Google Maps
+					style={styles.map}
+					region={{
+						latitude: 37.78825,
+						longitude: -122.4324,
+						latitudeDelta: 0.015,
+						longitudeDelta: 0.0121,
+					}}
+				>
+    		</MapView>
+  </View>
 			)
 	};
 }
 const styles = StyleSheet.create({
-	 container: {
-	   ...StyleSheet.absoluteFillObject,
-	   height: 400,
-	   width: 400,
-	   justifyContent: 'flex-end',
-	   alignItems: 'center',
-	 },
-	 map: {
-	   ...StyleSheet.absoluteFillObject,
-	 },
-	});
+	container: {
+		...StyleSheet.absoluteFillObject,
+		height: 400,
+		width: 400,
+		justifyContent: 'flex-end',
+		alignItems: 'center',
+	},
+	map: {
+		...StyleSheet.absoluteFillObject,
+	},
+ });
 
 
 export default  Home;
@@ -40,6 +49,10 @@ export default  Home;
 // ...
 
 /*
+<View style={styles.container}>
+			 
+<Text>Hello {this.props.name} </Text>
+   </View>
 
 <Text>HI KSKSK </Text>
      <MapView
