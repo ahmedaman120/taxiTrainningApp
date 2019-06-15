b1440ac47f21287d0ddf809590672d6fd63494ba
Ahmed ayman <ahmedayman.aa191@gmail.com>
2019-06-15 22:15:14 +0200

    cleanning

--- src/routes/Home/components/Home.js
+++ src/routes/Home/components/Home.js
@@ -1,8 +1,6 @@
 import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
-import MapView , {PROVIDER_GOOGLE}from 'react-native-maps'; // remove PROVIDER_GOOGLE import if not using Google Maps
-
-
+import MapCont from './MapComponent';
+import {Container} from 'native-base';
 
 class Home extends React.Component{
 		componentDidMount(){
@@ -10,37 +8,19 @@ class Home extends React.Component{
 	}
 	
 	render(){
-
+		const region ={
+			latitude: 37.78825,
+			longitude: -122.4324,
+			latitudeDelta: 0.015,
+			longitudeDelta: 0.0121,
+		}
 		return(
-			<View style={styles.container}>
-			<MapView
-					provider={PROVIDER_GOOGLE} // remove if not using Google Maps
-					style={styles.map}
-					region={{
-						latitude: 37.78825,
-						longitude: -122.4324,
-						latitudeDelta: 0.015,
-						longitudeDelta: 0.0121,
-					}}
-				>
-			</MapView>
-	</View>
-			)
+				<Container>
+					<MapCont region={region} />
+				</Container>
+				);
 	};
 }
-const styles = StyleSheet.create({
-	container: {
-		...StyleSheet.absoluteFillObject,
-		height: 400,
-		width: 400,
-		justifyContent: 'flex-end',
-		alignItems: 'center',
-	},
-	map: {
-		...StyleSheet.absoluteFillObject,
-	},
- });
-
 
 export default  Home;
 /*
---
+++ src/routes/Home/components/MapComponent/index.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import { View } from 'native-base';
+import MapView,{PROVIDER_GOOGLE} from 'react-native-maps';
+import styles from './MapContainerStyle';
+export default class MapCont extends Component {
+    
+    render() {
+        const region = this.props.region;
+        return (
+            <View style={styles.container}>
+                <MapView
+					provider={PROVIDER_GOOGLE} // remove if not using Google Maps
+					style={styles.map}
+					region={region}
+				>
+                </MapView>
+            </View>
+        )
+    }
+}
+
+/**{
+						latitude: 37.78825,
+						longitude: -122.4324,
+						latitudeDelta: 0.015,
+						longitudeDelta: 0.0121,
+					} */
